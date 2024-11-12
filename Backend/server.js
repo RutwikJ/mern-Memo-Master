df703767a7c00a3d88acf5952680679100fd0199
Rutwik <rutwik1996@gmail.com>
2024-11-12 18:54:41 +0530

    Setup CORS

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import connectDB from './Config/dbConn.config.js';
+import errorHandler from './middlware/errorHandler.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+import corsOptions from './Config/corsOptions.js';
 const app= express();
-const PORT=3000;
+const PORT=process.env.PORT || 3000;
+dotenv.config();
 
+connectDB()
+app.use(express.json());
+
+app.use(cookieParser())
+app.use(cors(corsOptions));
+
+app.use(errorHandler)
 app.listen(PORT,()=>{
     console.log(`Server is listening on Port ${PORT}`)
 });
-app.get('/',(req,res)=>{
-    res.send('aila jadu')
-})
